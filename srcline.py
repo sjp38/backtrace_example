be245e73c353e627b3592eae275cf80fde89d8b1
SeongJae Park <sj38.park@gmail.com>
2019-04-10 12:29:36 +0900

    Add a script for better readability of output
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ srcline.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+
+for line in sys.stdin:
+    spltd = line.split()
+    addr = spltd[-1][1:-1]
+    print spltd[0]
+    cmd = "addr2line -e program %s" % addr
+    print cmd
+    subprocess.call(cmd, shell=True, executable="/bin/bash")
