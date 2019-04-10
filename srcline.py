4de4445bff61ca9db9d71b24a85fb978b5a05feb
SeongJae Park <sj38.park@gmail.com>
2019-04-10 12:32:18 +0900

    srcline: Polish output
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- srcline.py
+++ srcline.py
@@ -7,6 +7,6 @@ import subprocess
 for line in sys.stdin:
     spltd = line.split()
     addr = spltd[-1][1:-1]
-    print spltd[0]
     cmd = "addr2line -e program %s" % addr
-    subprocess.call(cmd, shell=True, executable="/bin/bash")
+    srcline = subprocess.check_output(cmd, shell=True, executable="/bin/bash")
+    print spltd[0], srcline.strip()
